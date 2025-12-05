58a6ae912f3bc36ad88a0e5b682a31459adeb68e
Andres Court <andres@andrescourt.com>
2025-12-05 14:42:07 -0500

    feat(database): connect to bca's database

--- internal/database/database.go
+++ internal/database/database.go
@@ -1,15 +1,31 @@
 package database
 
 import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
 type Service interface{}
 
 type service struct {
+	db *sql.DB
 }
 
 func New() Service {
 	var db *sql.DB
+	var err error
+	connStr := os.Getenv("DATABASE_URL")
+	if connStr == "" {
+		fmt.Fprintf(os.Stderr, "New Router: DATABASE_URL is not set\n")
+		return nil
+	}
+	if db, err = sql.Open("pgx", connStr); err != nil {
+		fmt.Fprintf(os.Stderr, "New Database: Unable to connect to database: %v\n", err)
+		return nil
+	}
 	s := &service{
 		db: db,
 	}
