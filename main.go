b954fa9d098d7b3ed5baf757d198e23f9efb2902
Andres Court <andres@andrescourt.com>
2025-12-05 07:21:19 -0500

    task(http): open port from environment variable

--- main.go
+++ main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/alcb1310/final-bca-go/internal/router"
+	_ "github.com/joho/godotenv/autoload"
 )
+
+var port = os.Getenv("PORT")
 
 func main() {
 	r := router.NewRouter()
@@ -13,5 +17,8 @@ func main() {
 	r.GenerateRoutes()
 
 	fmt.Println("Server running on port 8080")
-	http.ListenAndServe(":8080", r.Router)
+	if err := http.ListenAndServe(fmt.Sprintf(":%s", port), r.Router); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %s\r\n", err)
+		os.Exit(1)
+	}
 }
