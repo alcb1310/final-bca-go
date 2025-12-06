dc55720b039881658de65bf43aaff6e4e6268cab
Andres Court <andres@andrescourt.com>
2025-12-05 22:15:34 -0500

    feat(routes): perform health check

--- internal/router/health.go
+++ internal/router/health.go
@@ -6,6 +6,12 @@ import (
 )
 
 func (rf *Router) HealthCheck(w http.ResponseWriter, r *http.Request) {
+	if !rf.DB.GetHealth() {
+		w.WriteHeader(http.StatusInternalServerError)
+		_ = json.NewEncoder(w).Encode(map[string]any{"message": "Unable to connect to the database, please check your logs"})
+		return
+	}
+
 	w.WriteHeader(http.StatusOK)
-	_ = json.NewEncoder(w).Encode(map[string]any{"message": "OK"})
+	_ = json.NewEncoder(w).Encode(map[string]any{"message": "The database is healthy"})
 }
