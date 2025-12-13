ef7ab52ca28380b050279517a0250e1ea90b79bd
Andres Court <andres@andrescourt.com>
2025-12-12 22:00:37 -0500

    feat(project): return confilict if project exists

--- internal/database/project.go
+++ internal/database/project.go
@@ -10,6 +10,7 @@ func (s *service) CreateProject(p types.Project) error {
 	sql := "insert into project (name, is_active, gross_area, net_area) values ($1, $2, $3, $4)"
 	if _, err := s.db.Exec(sql, p.Name, p.IsActive, p.GrossArea, p.NetArea); err != nil {
 		slog.Error("Error creating project", "err", err)
+		return err
 	}
 	return nil
 }
