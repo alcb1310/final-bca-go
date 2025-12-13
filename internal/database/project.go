77adeb12cccf51fdd0d0e186d70f67989a12b9da
Andres Court <andres@andrescourt.com>
2025-12-13 11:51:13 -0500

    feat(project): fetch one project from database

--- internal/database/project.go
+++ internal/database/project.go
@@ -4,12 +4,13 @@ import (
 	"log/slog"
 
 	"github.com/alcb1310/final-bca-go/internal/types"
+	"github.com/google/uuid"
 )
 
 func (s *service) CreateProject(p types.Project) error {
 	sql := "insert into project (name, is_active, gross_area, net_area) values ($1, $2, $3, $4)"
 	if _, err := s.db.Exec(sql, p.Name, p.IsActive, p.GrossArea, p.NetArea); err != nil {
-		slog.Error("Error creating project", "err", err)
+		slog.Error("CreateProject: Error creating project", "err", err)
 		return err
 	}
 	return nil
@@ -28,11 +29,25 @@ func (s *service) GetProjects() ([]types.Project, error) {
 	for rows.Next() {
 		var p types.Project
 		if err := rows.Scan(&p.Id, &p.Name, &p.IsActive, &p.GrossArea, &p.NetArea); err != nil {
-			slog.Error("Error scanning project", "err", err)
+			slog.Error("GetProjects: Error scanning project", "err", err)
 			return nil, err
 		}
 		projects = append(projects, p)
 	}
 
 	return projects, nil
+}
+
+func (s *service) GetProject(id uuid.UUID) (types.Project, error) {
+	var err error
+	project := types.Project{}
+
+	sql := "select id, name, is_active, gross_area, net_area from project where id = $1"
+	err = s.db.QueryRow(sql, id).Scan(&project.Id, &project.Name, &project.IsActive, &project.GrossArea, &project.NetArea)
+
+	if err != nil {
+		slog.Error("GetProject: Error scanning project", "err", err)
+	}
+
+	return project, err
 }
