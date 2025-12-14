c7adaa55e73c99d6ed3c5f740e99d6d117ed4217
Andres Court <andres@andrescourt.com>
2025-12-13 20:58:58 -0500

    test(project): validate we have an empty projects table

---
+++ tests/api_projects_test.go
@@ -0,0 +1,66 @@
+package tests
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/modules/postgres"
+	"github.com/testcontainers/testcontainers-go/wait"
+)
+
+func TestApiProjects(t *testing.T) {
+	ctx := context.Background()
+	testUrl := "/api/v2/projects"
+	path := filepath.Join("..", "schema", "tables.sql")
+	fmt.Println("filepath", path)
+
+	pgContainer, err := postgres.Run(ctx,
+		"postgres:18-alpine",
+		postgres.WithInitScripts(path),
+		postgres.WithDatabase("testbca"),
+		postgres.WithUsername("postgres"),
+		postgres.WithPassword("postgres"),
+		testcontainers.WithWaitStrategy(
+			wait.ForLog("database system is ready to accept connections").
+				WithOccurrence(2).
+				WithStartupTimeout(15*time.Second)),
+	)
+
+	assert.NoError(t, err)
+	if err != nil {
+		return
+	}
+
+	t.Cleanup(func() {
+		err := pgContainer.Terminate(ctx)
+		assert.NoError(t, err)
+	})
+
+	s, err := createServer(t, ctx, pgContainer)
+	assert.NoError(t, err)
+	if err != nil {
+		return
+	}
+	s.GenerateRoutes()
+
+	t.Run("should have no projects", func(t *testing.T) {
+		req, err := http.NewRequest("GET", testUrl, nil)
+		assert.NoError(t, err)
+		res := httptest.NewRecorder()
+		s.Router.ServeHTTP(res, req)
+
+		assert.Equal(t, http.StatusOK, res.Code)
+		var r []any
+		err = json.NewDecoder(res.Body).Decode(&r)
+		assert.NoError(t, err)
+		assert.Equal(t, 0, len(r))
+	})
+}
