33a2b80595276ae6c984f05781117ac1a750b903
Andres Court <andres@andrescourt.com>
2025-12-13 22:28:35 -0500

    test(project): should be able to create a project

--- tests/api_projects_test.go
+++ tests/api_projects_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"path/filepath"
@@ -64,5 +65,52 @@ func TestApiProjects(t *testing.T) {
 		assert.NoError(t, err)
 		assert.Equal(t, 0, len(r))
 		assert.Equal(t, "[]", strings.TrimSpace(res.Body.String()))
+	})
+
+	t.Run("should be able to create a project", func(t *testing.T) {
+		form := map[string]any{
+			"name":       "Project 1",
+			"is_active":  true,
+			"gross_area": 100.54,
+			"net_area":   150.54,
+		}
+
+		j, err := json.Marshal(form)
+		assert.NoError(t, err)
+
+		req, err := http.NewRequest(http.MethodPost, testUrl, strings.NewReader(string(j)))
+		assert.NoError(t, err)
+		req.Header.Set("Content-Type", "application/json")
+		res := httptest.NewRecorder()
+		s.Router.ServeHTTP(res, req)
+
+		assert.Equal(t, http.StatusCreated, res.Code)
+
+		body, err := io.ReadAll(res.Body)
+		assert.NoError(t, err)
+		mapBody := make(map[string]any)
+		err = json.Unmarshal(body, &mapBody)
+		assert.NoError(t, err)
+
+		assert.Equal(t, "Proyecto creado correctamente", mapBody["message"])
+
+		req, err = http.NewRequest("GET", testUrl, nil)
+		assert.NoError(t, err)
+		req.Header.Set("Content-Type", "application/json")
+		res = httptest.NewRecorder()
+		s.Router.ServeHTTP(res, req)
+
+		assert.Equal(t, http.StatusOK, res.Code)
+		var r []any
+		err = json.Unmarshal(res.Body.Bytes(), &r)
+		assert.NoError(t, err)
+		assert.Equal(t, 1, len(r))
+
+		for _, v := range r {
+			assert.Equal(t, "Project 1", v.(map[string]any)["name"])
+			assert.Equal(t, true, v.(map[string]any)["is_active"])
+			assert.Equal(t, 100.54, v.(map[string]any)["gross_area"])
+			assert.Equal(t, 150.54, v.(map[string]any)["net_area"])
+		}
 	})
 }
