6a2c92560036b038ffb67f9c5369160a7bff5ad6
Andres Court <andres@andrescourt.com>
2025-12-15 21:29:06 -0500

    feat(suppliers): validate input fields

--- internal/router/suppliers.go
+++ internal/router/suppliers.go
@@ -3,6 +3,8 @@ package router
 import (
 	"encoding/json"
 	"net/http"
+
+	"github.com/alcb1310/final-bca-go/internal/types"
 )
 
 func (rf *Router) GetSuppliers(w http.ResponseWriter, r *http.Request) {
@@ -18,5 +20,48 @@ func (rf *Router) GetSuppliers(w http.ResponseWriter, r *http.Request) {
 }
 
 func (rf *Router) CreateSupplier(w http.ResponseWriter, r *http.Request) {
+	errorResponse := make(map[string]any)
+	p := make(map[string]any)
+	var supplier types.Supplier
+	var err error
+	var ok bool
+
+	if r.Body == http.NoBody || r.Body == nil {
+		w.WriteHeader(http.StatusUnprocessableEntity)
+		errorResponse["message"] = "Falta el cuerpo de la solicitud"
+		_ = json.NewEncoder(w).Encode(errorResponse)
+		return
+	}
+
+	if err = json.NewDecoder(r.Body).Decode(&p); err != nil {
+		errorResponse["message"] = "Cuerpo de la solicitud no válido"
+	}
+
+	if supplier.Name, ok = p["name"].(string); !ok {
+		errorResponse["name"] = "El nombre es obligatorio"
+	} else if len(supplier.Name) == 0 {
+		errorResponse["name"] = "El nombre es obligatorio"
+	}
+
+	if supplier.SupplierId, ok = p["supplier_id"].(string); !ok {
+		errorResponse["supplier_id"] = "El nombre es obligatorio"
+	} else if len(supplier.SupplierId) == 0 {
+		errorResponse["supplier_id"] = "El nombre es obligatorio"
+	}
+
+	var val string
+
+	if val, ok = p["contact_email"].(string); !ok {
+		supplier.ContactEmail.Valid = false
+	} else {
+		supplier.ContactEmail.Valid = true
+		supplier.ContactEmail.String = val
+	}
+
+	if len(errorResponse) > 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		_ = json.NewEncoder(w).Encode(errorResponse)
+		return
+	}
 	w.WriteHeader(http.StatusNotImplemented)
 }
