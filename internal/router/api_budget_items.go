a65e9aa30669bf6a19312f626181216fc1618726
Andres Court <andres@andrescourt.com>
2025-12-20 17:32:42 -0500

    feat(budget-items): validate id to be uuid

--- internal/router/api_budget_items.go
+++ internal/router/api_budget_items.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	"github.com/alcb1310/final-bca-go/internal/types"
+	"github.com/go-chi/chi/v5"
 	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5/pgconn"
 )
@@ -95,5 +96,14 @@ func (rf *Router) CreateBudgetItem(w http.ResponseWriter, r *http.Request) {
 }
 
 func (rf *Router) UpdateBudgetItem(w http.ResponseWriter, r *http.Request) {
+	pId := chi.URLParam(r, "id")
+	parsedId, err := uuid.Parse(pId)
+	if err != nil {
+		w.WriteHeader(http.StatusNotAcceptable)
+		_ = json.NewEncoder(w).Encode(map[string]any{"message": "Id inválido"})
+		return
+	}
+
 	w.WriteHeader(http.StatusNotImplemented)
+	_ = json.NewEncoder(w).Encode(map[string]any{"message": "Not implemented", "id": parsedId})
 }
