b006bb3a767f1f4468fd13beb4613c2ce96909c4
Andres Court <andres@andrescourt.com>
2025-12-19 23:15:17 -0500

    feat(budget-items): get all budget-items route implementation

--- internal/router/api_budget_items.go
+++ internal/router/api_budget_items.go
@@ -1,7 +1,23 @@
 package router
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/alcb1310/final-bca-go/internal/types"
+)
 
 func (rf *Router) GetBudgetItems(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusNotImplemented)
+	var bi []types.BudgetItem
+	var err error
+
+	if bi, err = rf.DB.GetBudgetItems(); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		res := map[string]any{"message": err.Error()}
+		_ = json.NewEncoder(w).Encode(res)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	_ = json.NewEncoder(w).Encode(bi)
 }
