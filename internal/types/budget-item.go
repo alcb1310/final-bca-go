f23b1e2bd5b0a58ae2dcc0b27ab6121529108e3f
Andres Court <andres@andrescourt.com>
2025-12-19 23:01:20 -0500

    feat(budget-items): budget-item response type

---
+++ internal/types/budget-item.go
@@ -0,0 +1,18 @@
+package types
+
+import (
+	"database/sql"
+
+	"github.com/google/uuid"
+)
+
+type BudgetItem struct {
+	Id         uuid.UUID      `json:"id"`
+	Code       string         `json:"code"`
+	Name       string         `json:"name"`
+	Level      uint8          `json:"level"`
+	Accumulate bool           `json:"accumulate"`
+	ParentId   uuid.NullUUID  `json:"parentId"`
+	ParentCode sql.NullString `json:"parentCode"`
+	ParentName sql.NullString `json:"parentName"`
+}
