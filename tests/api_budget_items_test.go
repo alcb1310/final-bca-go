588dc8ba19ae82e6bd5935f978be32a035579a0d
Andres Court <andres@andrescourt.com>
2025-12-20 20:22:11 -0500

    test(budget-items): should return not exist when non existent supplier

--- tests/api_budget_items_test.go
+++ tests/api_budget_items_test.go
@@ -12,6 +12,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 	"github.com/testcontainers/testcontainers-go"
 	"github.com/testcontainers/testcontainers-go/modules/postgres"
@@ -131,5 +132,31 @@ func TestApiBudgetItems(t *testing.T) {
 		s.Router.ServeHTTP(res, req)
 
 		assert.Equal(t, http.StatusConflict, res.Code)
+	})
+
+	t.Run("should return not exist when non existent supplier", func(t *testing.T) {
+		form := map[string]any{
+			"code": "100",
+			"name": "Prueba",
+		}
+
+		j, err := json.Marshal(form)
+		assert.NoError(t, err)
+		id := uuid.New()
+
+		req, err := http.NewRequest(http.MethodPut, fmt.Sprintf("%s/%s", testUrl, id), strings.NewReader(string(j)))
+		assert.NoError(t, err)
+		req.Header.Set("Content-Type", "application/json")
+		res := httptest.NewRecorder()
+		s.Router.ServeHTTP(res, req)
+
+		assert.Equal(t, http.StatusNotFound, res.Code)
+		body, err := io.ReadAll(res.Body)
+		assert.NoError(t, err)
+		mapBody := make(map[string]any)
+		err = json.Unmarshal(body, &mapBody)
+		assert.NoError(t, err)
+
+		assert.Equal(t, "Partida no encontrada", mapBody["message"])
 	})
 }
