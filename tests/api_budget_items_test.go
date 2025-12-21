05165ee7684991b0cf3c94072e2e64ad9ed8c5c2
Andres Court <andres@andrescourt.com>
2025-12-20 19:16:54 -0500

    test(budget-items): should have no budget-items

---
+++ tests/api_budget_items_test.go
@@ -0,0 +1,68 @@
+package tests
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"path/filepath"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/modules/postgres"
+	"github.com/testcontainers/testcontainers-go/wait"
+)
+
+func TestApiBudgetItems(t *testing.T) {
+	ctx := context.Background()
+	testUrl := "/api/v2/budget-items"
+	fmt.Println(testUrl)
+	path := filepath.Join("..", "schema", "tables.sql")
+
+	pgContainer, err := postgres.Run(ctx,
+		"postgres:18-alpine",
+		postgres.WithInitScripts(path),
+		postgres.WithDatabase("testbca"),
+		postgres.WithUsername("postgres"),
+		postgres.WithPassword("postgres"),
+		testcontainers.WithWaitStrategy(
+			wait.ForLog("database system is ready to accept connections").
+				WithOccurrence(2).
+				WithStartupTimeout(15*time.Second)),
+	)
+
+	assert.NoError(t, err)
+	if err != nil {
+		return
+	}
+
+	t.Cleanup(func() {
+		err := pgContainer.Terminate(ctx)
+		assert.NoError(t, err)
+	})
+
+	s, err := createServer(t, ctx, pgContainer)
+	assert.NoError(t, err)
+	if err != nil {
+		return
+	}
+	s.GenerateRoutes()
+
+	t.Run("should have no budget items", func(t *testing.T) {
+		req, err := http.NewRequest("GET", testUrl, nil)
+		assert.NoError(t, err)
+		res := httptest.NewRecorder()
+		s.Router.ServeHTTP(res, req)
+
+		assert.Equal(t, http.StatusOK, res.Code)
+		var r []any
+		err = json.Unmarshal(res.Body.Bytes(), &r)
+		assert.NoError(t, err)
+		assert.Equal(t, 0, len(r))
+		assert.Equal(t, "[]", strings.TrimSpace(res.Body.String()))
+	})
+}
