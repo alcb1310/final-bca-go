106710658c04ed4f3b3aef326a9f8931aa522e06
Andres Court <andres@andrescourt.com>
2025-12-23 14:50:13 -0500

    feat(categories): return a list of all the categories

---
+++ internal/router/api_categories.go
@@ -0,0 +1,23 @@
+package router
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/alcb1310/final-bca-go/internal/types"
+)
+
+func (rf *Router) GetCategories(w http.ResponseWriter, r *http.Request) {
+	var categories []types.Category
+	var err error
+
+	categories, err = rf.DB.GetCategories()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		_ = json.NewEncoder(w).Encode(err)
+		return
+	}
+
+	w.WriteHeader(http.StatusNotImplemented)
+	_ = json.NewEncoder(w).Encode(categories)
+}
