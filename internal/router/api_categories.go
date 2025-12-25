474124f9feb2deb515490a5e1b980c2160212893
Andres Court <andres@andrescourt.com>
2025-12-25 10:08:26 -0500

    feat(category): user's input validation

--- internal/router/api_categories.go
+++ internal/router/api_categories.go
@@ -23,5 +23,34 @@ func (rf *Router) GetCategories(w http.ResponseWriter, r *http.Request) {
 }
 
 func (rf *Router) CreateCategory(w http.ResponseWriter, r *http.Request) {
+	errorResponse := make(map[string]any)
+	p := make(map[string]any)
+	var category types.Category
+	var err error
+	var ok bool
+
+	if r.Body == http.NoBody || r.Body == nil {
+		w.WriteHeader(http.StatusUnprocessableEntity)
+		errorResponse["message"] = "Falta el cuerpo de la solicitud"
+		_ = json.NewEncoder(w).Encode(errorResponse)
+		return
+	}
+
+	if err = json.NewDecoder(r.Body).Decode(&p); err != nil {
+		errorResponse["message"] = "Cuerpo de la solicitud no válido"
+	}
+
+	if category.Name, ok = p["name"].(string); !ok {
+		errorResponse["code"] = "El nombre es obligatorio"
+	} else if len(category.Name) == 0 {
+		errorResponse["code"] = "El nombre es obligatorio"
+	}
+
+	if len(errorResponse) > 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		_ = json.NewEncoder(w).Encode(errorResponse)
+		return
+	}
+
 	w.WriteHeader(http.StatusNotImplemented)
 }
