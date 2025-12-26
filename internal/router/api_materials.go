3444eb3daf06597457220612caabfd5a6f965a0d
Andres Court <andres@andrescourt.com>
2025-12-26 18:58:06 -0500

    feat(materials): read the materials from the database

--- internal/router/api_materials.go
+++ internal/router/api_materials.go
@@ -1,7 +1,17 @@
 package router
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
 
 func (rf *Router) GetMaterials(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusNotImplemented)
+	materials, err := rf.DB.GetMaterials()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		_ = json.NewEncoder(w).Encode(err)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	_ = json.NewEncoder(w).Encode(materials)
 }
