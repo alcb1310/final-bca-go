e0db0ca2933e5a7e37167baf6708c6cc8d23ea5c
Andres Court <andres@andrescourt.com>
2025-12-25 19:10:22 -0500

    test(category): should conflict

--- tests/api_category_test.go
+++ tests/api_category_test.go
@@ -105,4 +105,21 @@ func TestApiCategoryTest(t *testing.T) {
 			assert.Equal(t, "Prueba", v.(map[string]any)["name"])
 		}
 	})
+
+	t.Run("should show conflict error", func(t *testing.T) {
+		form := map[string]any{
+			"name": "Prueba",
+		}
+
+		j, err := json.Marshal(form)
+		assert.NoError(t, err)
+
+		req, err := http.NewRequest("POST", testUrl, strings.NewReader(string(j)))
+		assert.NoError(t, err)
+		req.Header.Set("Content-Type", "application/json")
+		res := httptest.NewRecorder()
+		s.Router.ServeHTTP(res, req)
+
+		assert.Equal(t, http.StatusConflict, res.Code)
+	})
 }
