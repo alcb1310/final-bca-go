f336a0e3554580b2fc6027e314d4b21c7236e5fe
Andres Court <andres@andrescourt.com>
2025-12-27 18:56:43 -0500

    test(materials): should be able to create a material

--- tests/api_materials_test.go
+++ tests/api_materials_test.go
@@ -3,6 +3,7 @@ package tests
 import (
 	"context"
 	"encoding/json"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"path/filepath"
@@ -64,5 +65,52 @@ func TestApiMaterialsTest(t *testing.T) {
 		assert.NoError(t, err)
 		assert.Equal(t, 0, len(r))
 		assert.Equal(t, "[]", strings.TrimSpace(res.Body.String()))
+	})
+
+	t.Run("should be able to create a material", func(t *testing.T) {
+		form := map[string]any{
+			"name":        "Prueba",
+			"code":        "PRU",
+			"unit":        "Unidad",
+			"category_id": "325a7622-53dd-4ccf-b109-4f98b8daac55",
+		}
+
+		j, err := json.Marshal(form)
+		assert.NoError(t, err)
+
+		req, err := http.NewRequest("POST", testUrl, strings.NewReader(string(j)))
+		assert.NoError(t, err)
+		req.Header.Set("Content-Type", "application/json")
+		res := httptest.NewRecorder()
+		s.Router.ServeHTTP(res, req)
+
+		assert.Equal(t, http.StatusCreated, res.Code)
+
+		body, err := io.ReadAll(res.Body)
+		assert.NoError(t, err)
+		mapBody := make(map[string]any)
+		err = json.Unmarshal(body, &mapBody)
+		assert.NoError(t, err)
+
+		assert.Equal(t, "Material creado", mapBody["message"])
+
+		req, err = http.NewRequest("GET", testUrl, nil)
+		assert.NoError(t, err)
+		req.Header.Set("Content-Type", "application/json")
+		res = httptest.NewRecorder()
+		s.Router.ServeHTTP(res, req)
+
+		var r []any
+		err = json.Unmarshal(res.Body.Bytes(), &r)
+		assert.NoError(t, err)
+		assert.Equal(t, 1, len(r))
+		for _, v := range r {
+			assert.Equal(t, "Prueba", v.(map[string]any)["name"])
+			assert.Equal(t, "PRU", v.(map[string]any)["code"])
+			assert.Equal(t, "Unidad", v.(map[string]any)["unit"])
+			cat := v.(map[string]any)["category"].(map[string]any)
+			assert.Equal(t, "325a7622-53dd-4ccf-b109-4f98b8daac55", cat["id"])
+			assert.Equal(t, "prueba", cat["name"])
+		}
 	})
 }
