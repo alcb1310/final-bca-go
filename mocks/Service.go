eaefcd9e1911cd6a88c8a08abe549367020180ec
Andres Court <andres@andrescourt.com>
2025-12-27 16:58:28 -0500

    test(materials): update database service mocks

--- mocks/Service.go
+++ mocks/Service.go
@@ -1011,6 +1011,52 @@ func (_c *Service_UpdateCategory_Call) RunAndReturn(run func(types.Category) err
 	return _c
 }
 
+// UpdateMaterial provides a mock function with given fields: mat
+func (_m *Service) UpdateMaterial(mat types.Materials) error {
+	ret := _m.Called(mat)
+
+	if len(ret) == 0 {
+		panic("no return value specified for UpdateMaterial")
+	}
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(types.Materials) error); ok {
+		r0 = rf(mat)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
+}
+
+// Service_UpdateMaterial_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'UpdateMaterial'
+type Service_UpdateMaterial_Call struct {
+	*mock.Call
+}
+
+// UpdateMaterial is a helper method to define mock.On call
+//   - mat types.Materials
+func (_e *Service_Expecter) UpdateMaterial(mat interface{}) *Service_UpdateMaterial_Call {
+	return &Service_UpdateMaterial_Call{Call: _e.mock.On("UpdateMaterial", mat)}
+}
+
+func (_c *Service_UpdateMaterial_Call) Run(run func(mat types.Materials)) *Service_UpdateMaterial_Call {
+	_c.Call.Run(func(args mock.Arguments) {
+		run(args[0].(types.Materials))
+	})
+	return _c
+}
+
+func (_c *Service_UpdateMaterial_Call) Return(_a0 error) *Service_UpdateMaterial_Call {
+	_c.Call.Return(_a0)
+	return _c
+}
+
+func (_c *Service_UpdateMaterial_Call) RunAndReturn(run func(types.Materials) error) *Service_UpdateMaterial_Call {
+	_c.Call.Return(run)
+	return _c
+}
+
 // UpdateProject provides a mock function with given fields: p
 func (_m *Service) UpdateProject(p types.Project) error {
 	ret := _m.Called(p)
