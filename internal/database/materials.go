84a0614f512dae3ab0bc9e1935c1491f3b52648c
Andres Court <andres@andrescourt.com>
2025-12-27 11:27:53 -0500

    feat(materials): get one material query

--- internal/database/materials.go
+++ internal/database/materials.go
@@ -1,6 +1,9 @@
 package database
 
-import "github.com/alcb1310/final-bca-go/internal/types"
+import (
+	"github.com/alcb1310/final-bca-go/internal/types"
+	"github.com/google/uuid"
+)
 
 func (s *service) GetMaterials() ([]types.Materials, error) {
 	materials := []types.Materials{}
@@ -27,4 +30,12 @@ func (s *service) CreateMaterial(mat types.Materials) error {
 	sql := "insert into material (code, name, unit, category_id) values ($1, $2, $3, $4)"
 	_, err := s.db.Exec(sql, mat.Code, mat.Name, mat.Unit, mat.Category.Id)
 	return err
+}
+
+func (s *service) GetMaterial(id uuid.UUID) (types.Materials, error) {
+	material := types.Materials{}
+
+	sql := "select id, code, name, unit, category_id, category_name from vw_materials where id = $1"
+	err := s.db.QueryRow(sql, id).Scan(&material.Id, &material.Code, &material.Name, &material.Unit, &material.Category.Id, &material.Category.Name)
+	return material, err
 }
