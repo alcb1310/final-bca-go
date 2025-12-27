a04c9372783399fc9b76dc53e5a1adc3f015cca7
Andres Court <andres@andrescourt.com>
2025-12-27 17:57:01 -0500

    test(materials): create the integratin tests

---
+++ tests/api_materials_test.go
@@ -0,0 +1,50 @@
+package tests
+
+import (
+	"context"
+	"fmt"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/modules/postgres"
+	"github.com/testcontainers/testcontainers-go/wait"
+)
+
+func TestApiMaterialsTest(t *testing.T) {
+	ctx := context.Background()
+	testUrl := "/api/v2/categories"
+	fmt.Println(testUrl)
+	path := filepath.Join("..", "schema", "tables.sql")
+
+	pgContainer, err := postgres.Run(ctx,
+		"postgres:18-alpine",
+		postgres.WithInitScripts(path),
+		postgres.WithDatabase("testbca"),
+		postgres.WithUsername("postgres"),
+		postgres.WithPassword("postgres"),
+		testcontainers.WithWaitStrategy(
+			wait.ForLog("database system is ready to accept connections").
+				WithOccurrence(2).
+				WithStartupTimeout(15*time.Second)),
+	)
+
+	assert.NoError(t, err)
+	if err != nil {
+		return
+	}
+
+	t.Cleanup(func() {
+		err := pgContainer.Terminate(ctx)
+		assert.NoError(t, err)
+	})
+
+	s, err := createServer(t, ctx, pgContainer)
+	assert.NoError(t, err)
+	if err != nil {
+		return
+	}
+	s.GenerateRoutes()
+}
