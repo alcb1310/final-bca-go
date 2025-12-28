ca1c6158def38b20113f92dba461d50ffa1f225d
Andres Court <andres@andrescourt.com>
2025-12-28 18:32:22 -0500

    feat(items): list all items route version

--- internal/router/api_items.go
+++ internal/router/api_items.go
@@ -1,7 +1,22 @@
 package router
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/alcb1310/final-bca-go/internal/types"
+)
 
 func (rf *Router) GetItems(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusNotImplemented)
+	items := []types.Items{}
+	var err error
+
+	if items, err = rf.DB.GetItems(); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		_ = json.NewEncoder(w).Encode(err)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	_ = json.NewEncoder(w).Encode(items)
 }
