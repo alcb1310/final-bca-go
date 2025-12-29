47676e7b2baaf442ef253f2fc451e075732d83e5
Andres Court <andres@andrescourt.com>
2025-12-28 21:19:27 -0500

    feat(items): read and validate user input

--- internal/router/api_items.go
+++ internal/router/api_items.go
@@ -22,5 +22,47 @@ func (rf *Router) GetItems(w http.ResponseWriter, r *http.Request) {
 }
 
 func (rf *Router) CreateItem(w http.ResponseWriter, r *http.Request) {
+	errorResponse := make(map[string]any)
+	p := make(map[string]any)
+	var item types.Items
+	var err error
+	var ok bool
+
+	if r.Body == http.NoBody || r.Body == nil {
+		w.WriteHeader(http.StatusUnprocessableEntity)
+		errorResponse["message"] = "Falta el cuerpo de la solicitud"
+		_ = json.NewEncoder(w).Encode(errorResponse)
+		return
+	}
+
+	if err = json.NewDecoder(r.Body).Decode(&p); err != nil {
+		errorResponse["message"] = "Cuerpo de la solicitud no válido"
+	}
+
+	if item.Code, ok = p["code"].(string); !ok {
+		errorResponse["code"] = "El código es obligatorio"
+	} else if len(item.Code) == 0 {
+		errorResponse["code"] = "El código es obligatorio"
+	}
+
+	if item.Name, ok = p["name"].(string); !ok {
+		errorResponse["name"] = "El nombre es obligatorio"
+	} else if len(item.Name) == 0 {
+		errorResponse["name"] = "El nombre es obligatorio"
+	}
+
+	if item.Unit, ok = p["unit"].(string); !ok {
+		errorResponse["unit"] = "La unidad es obligatoria"
+	} else if len(item.Unit) == 0 {
+		errorResponse["unit"] = "La unidad es obligatoria"
+	}
+
+	if len(errorResponse) > 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		_ = json.NewEncoder(w).Encode(errorResponse)
+		return
+	}
+
 	w.WriteHeader(http.StatusNotImplemented)
+	_ = json.NewEncoder(w).Encode(item)
 }
