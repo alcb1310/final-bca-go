053c5deacdc15b03758b63f7d0d71c4a4fbffe19
Andres Court <andres@andrescourt.com>
2025-12-29 12:08:24 -0500

    feat(items): find the item based on the id passed

--- internal/router/api_items.go
+++ internal/router/api_items.go
@@ -1,6 +1,7 @@
 package router
 
 import (
+	"database/sql"
 	"encoding/json"
 	"errors"
 	"net/http"
@@ -93,6 +94,19 @@ func (rf *Router) UpdateItem(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	item, err := rf.DB.GetItem(parsedId)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			w.WriteHeader(http.StatusNotFound)
+			_ = json.NewEncoder(w).Encode(map[string]any{"message": "Item no encontrado"})
+			return
+		}
+
+		w.WriteHeader(http.StatusInternalServerError)
+		_ = json.NewEncoder(w).Encode(err)
+		return
+	}
+
 	w.WriteHeader(http.StatusNotImplemented)
-	_ = json.NewEncoder(w).Encode(map[string]any{"message": "Not implemented", "id": parsedId})
+	_ = json.NewEncoder(w).Encode(map[string]any{"message": "Not implemented", "item": item})
 }
