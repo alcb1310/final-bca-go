094a34409ad9885a6d09812fa124bc4770b7384a
Andres Court <andres@andrescourt.com>
2025-12-29 12:32:14 -0500

    test(items): create the integration test suite

---
+++ tests/api_items_test.go
@@ -0,0 +1,49 @@
+package tests
+
+import (
+	"context"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/modules/postgres"
+	"github.com/testcontainers/testcontainers-go/wait"
+)
+
+func TestApiItemsTest(t *testing.T) {
+	ctx := context.Background()
+	testUrl := "/api/v2/categories"
+	t.Log(testUrl)
+	path := filepath.Join("..", "schema", "tables.sql")
+
+	pgContainer, err := postgres.Run(ctx,
+		"postgres:18-alpine",
+		postgres.WithInitScripts(path),
+		postgres.WithDatabase("testbca"),
+		postgres.WithUsername("postgres"),
+		postgres.WithPassword("postgres"),
+		testcontainers.WithWaitStrategy(
+			wait.ForLog("database system is ready to accept connections").
+				WithOccurrence(2).
+				WithStartupTimeout(15*time.Second)),
+	)
+
+	assert.NoError(t, err)
+	if err != nil {
+		return
+	}
+
+	t.Cleanup(func() {
+		err := pgContainer.Terminate(ctx)
+		assert.NoError(t, err)
+	})
+
+	s, err := createServer(t, ctx, pgContainer)
+	assert.NoError(t, err)
+	if err != nil {
+		return
+	}
+	s.GenerateRoutes()
+}
