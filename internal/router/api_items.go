a8e3620f9236449e3cc29ea4acc21c607ed096fa
Andres Court <andres@andrescourt.com>
2025-12-28 21:26:43 -0500

    feat(item): call the create item database method

--- internal/router/api_items.go
+++ internal/router/api_items.go
@@ -2,9 +2,11 @@ package router
 
 import (
 	"encoding/json"
+	"errors"
 	"net/http"
 
 	"github.com/alcb1310/final-bca-go/internal/types"
+	"github.com/jackc/pgx/v5/pgconn"
 )
 
 func (rf *Router) GetItems(w http.ResponseWriter, r *http.Request) {
@@ -63,6 +65,19 @@ func (rf *Router) CreateItem(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.WriteHeader(http.StatusNotImplemented)
-	_ = json.NewEncoder(w).Encode(item)
+	if err = rf.DB.CreateItem(item); err != nil {
+		var e *pgconn.PgError
+		if errors.As(err, &e) && e.Code == "23505" {
+			w.WriteHeader(http.StatusConflict)
+			_ = json.NewEncoder(w).Encode(map[string]any{"message": "El item ya existe"})
+			return
+		}
+
+		w.WriteHeader(http.StatusInternalServerError)
+		_ = json.NewEncoder(w).Encode(err)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	_ = json.NewEncoder(w).Encode(map[string]any{"message": "Item creado"})
 }
