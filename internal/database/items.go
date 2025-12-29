50ebe206dafd107a1695c870c77289c160107236
Andres Court <andres@andrescourt.com>
2025-12-28 21:20:55 -0500

    feat(items): create item database query

--- internal/database/items.go
+++ internal/database/items.go
@@ -22,3 +22,9 @@ func (s *service) GetItems() ([]types.Items, error) {
 
 	return items, nil
 }
+
+func (s *service) CreateItem(item types.Items) error {
+	sql := "insert into item (code, name, unit) values ($1, $2, $3)"
+	_, err := s.db.Exec(sql, item.Code, item.Name, item.Unit)
+	return err
+}
