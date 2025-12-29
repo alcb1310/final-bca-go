b7c08e6e03443740b8910cf7b8caf8bcf5bd86bf
Andres Court <andres@andrescourt.com>
2025-12-29 16:26:16 -0500

    test(items): should returnnot exist when non existent rubro

--- tests/api_items_test.go
+++ tests/api_items_test.go
@@ -3,6 +3,7 @@ package tests
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 	"net/http/httptest"
@@ -11,6 +12,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 	"github.com/testcontainers/testcontainers-go"
 	"github.com/testcontainers/testcontainers-go/modules/postgres"
@@ -134,5 +136,32 @@ func TestApiItemsTest(t *testing.T) {
 		assert.NoError(t, err)
 
 		assert.Equal(t, "El rubro ya existe", mapBody["message"])
+	})
+
+	t.Run("should return not exist when non existent rubro", func(t *testing.T) {
+		form := map[string]any{
+			"code": "100",
+			"name": "Test",
+			"unit": "u",
+		}
+
+		j, err := json.Marshal(form)
+		assert.NoError(t, err)
+		id := uuid.New()
+
+		req, err := http.NewRequest(http.MethodPut, fmt.Sprintf("%s/%s", testUrl, id), strings.NewReader(string(j)))
+		assert.NoError(t, err)
+		req.Header.Set("Content-Type", "application/json")
+		res := httptest.NewRecorder()
+		s.Router.ServeHTTP(res, req)
+
+		assert.Equal(t, http.StatusNotFound, res.Code)
+		body, err := io.ReadAll(res.Body)
+		assert.NoError(t, err)
+		mapBody := make(map[string]any)
+		err = json.Unmarshal(body, &mapBody)
+		assert.NoError(t, err)
+
+		assert.Equal(t, "Rubro no encontrado", mapBody["message"])
 	})
 }
