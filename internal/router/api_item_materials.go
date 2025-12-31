3ac52292335dfa54307e2caad474653813916efe
Andres Court <andres@andrescourt.com>
2025-12-31 14:50:57 -0500

    feat(item-materials): read the rubro id

--- internal/router/api_item_materials.go
+++ internal/router/api_item_materials.go
@@ -1,7 +1,36 @@
 package router
 
-import "net/http"
+import (
+	"database/sql"
+	"encoding/json"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/google/uuid"
+)
 
 func (rf *Router) GetItemMaterials(w http.ResponseWriter, r *http.Request) {
+	pId := chi.URLParam(r, "id")
+	parsedId, err := uuid.Parse(pId)
+	if err != nil {
+		w.WriteHeader(http.StatusNotAcceptable)
+		_ = json.NewEncoder(w).Encode(map[string]any{"message": "Id inválido"})
+		return
+	}
+
+	item, err := rf.DB.GetItem(parsedId)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			w.WriteHeader(http.StatusNotFound)
+			_ = json.NewEncoder(w).Encode(map[string]any{"message": "Rubro no encontrado"})
+			return
+		}
+
+		w.WriteHeader(http.StatusInternalServerError)
+		_ = json.NewEncoder(w).Encode(err)
+		return
+	}
+
 	w.WriteHeader(http.StatusNotImplemented)
+	_ = json.NewEncoder(w).Encode(item)
 }
