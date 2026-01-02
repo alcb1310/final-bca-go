d7e58b3423de24d0958e328af2cc1f498d245189
Andres Court <andres@andrescourt.com>
2026-01-01 21:23:49 -0500

    feat(item-materials): parse the item id url param

--- internal/router/api_item-materials.go
+++ internal/router/api_item-materials.go
@@ -224,5 +224,26 @@ func (rf *Router) UpdateItemMaterial(w http.ResponseWriter, r *http.Request) {
 }
 
 func (rf *Router) DeleteItemMaterial(w http.ResponseWriter, r *http.Request) {
+	pId := chi.URLParam(r, "id")
+	parsedId, err := uuid.Parse(pId)
+	if err != nil {
+		w.WriteHeader(http.StatusNotAcceptable)
+		_ = json.NewEncoder(w).Encode(map[string]any{"message": "Id inválido"})
+		return
+	}
+
+	_, err = rf.DB.GetItem(parsedId)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			w.WriteHeader(http.StatusNotFound)
+			_ = json.NewEncoder(w).Encode(map[string]any{"message": "Rubro no encontrado"})
+			return
+		}
+
+		w.WriteHeader(http.StatusInternalServerError)
+		_ = json.NewEncoder(w).Encode(err)
+		return
+	}
+
 	w.WriteHeader(http.StatusNotImplemented)
 }
