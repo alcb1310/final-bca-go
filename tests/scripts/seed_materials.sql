969cab156eb0aa0f92574a67c9264954af1f6a48
Andres Court <andres@andrescourt.com>
2026-01-02 12:04:23 -0500

    test(item-materials): added the material test seed data

---
+++ tests/scripts/seed_materials.sql
@@ -0,0 +1,13 @@
+INSERT INTO material (
+    id,
+    name,
+    code,
+    unit,
+    category_id
+) VALUES (
+    'b3fba400-acad-40a6-9ca3-17871151bc0f',
+    'prueba',
+    'prueba',
+    'prueba',
+    '325a7622-53dd-4ccf-b109-4f98b8daac55'
+)
