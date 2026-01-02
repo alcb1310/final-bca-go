d99ebc88bb2d514366964fd77696ff67b29c602d
Andres Court <andres@andrescourt.com>
2026-01-02 12:15:04 -0500

    test(item-materials): should have no rubro material

--- tests/api_item-materials_test.go
+++ tests/api_item-materials_test.go
@@ -2,7 +2,11 @@ package tests
 
 import (
 	"context"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
 	"path/filepath"
+	"strings"
 	"testing"
 	"time"
 
@@ -50,4 +54,19 @@ func TestApiItemMaterialsTest(t *testing.T) {
 		return
 	}
 	s.GenerateRoutes()
+
+	t.Run("should have no rubro materials", func(t *testing.T) {
+		req, err := http.NewRequest("GET", testUrl, nil)
+		assert.NoError(t, err)
+		res := httptest.NewRecorder()
+		s.Router.ServeHTTP(res, req)
+
+		assert.Equal(t, http.StatusOK, res.Code)
+		var r []any
+		err = json.Unmarshal(res.Body.Bytes(), &r)
+		assert.NoError(t, err)
+		assert.Equal(t, 0, len(r))
+		assert.Equal(t, "[]", strings.TrimSpace(res.Body.String()))
+	})
 }
+
