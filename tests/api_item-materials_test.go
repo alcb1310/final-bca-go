56c4796558fc5503fcbfa81d2b3d3a3fc214ccbb
Andres Court <andres@andrescourt.com>
2026-01-02 22:40:48 -0500

    test(item-material): should show conflict error

--- tests/api_item-materials_test.go
+++ tests/api_item-materials_test.go
@@ -119,5 +119,31 @@ func TestApiItemMaterialsTest(t *testing.T) {
 			assert.Equal(t, "pri", v.(map[string]any)["item_unit"])
 			assert.Equal(t, 32.5, v.(map[string]any)["quantity"])
 		}
+
+		t.Run("should show conflict error", func(t *testing.T) {
+			form := map[string]any{
+				"material_id": "b3fba400-acad-40a6-9ca3-17871151bc0f",
+				"quantity":    32.5,
+			}
+
+			j, err := json.Marshal(form)
+			assert.NoError(t, err)
+
+			req, err := http.NewRequest("POST", testUrl, strings.NewReader(string(j)))
+			assert.NoError(t, err)
+			req.Header.Set("Content-Type", "application/json")
+			res := httptest.NewRecorder()
+			s.Router.ServeHTTP(res, req)
+
+			assert.Equal(t, http.StatusConflict, res.Code)
+
+			body, err := io.ReadAll(res.Body)
+			assert.NoError(t, err)
+			mapBody := make(map[string]any)
+			err = json.Unmarshal(body, &mapBody)
+			assert.NoError(t, err)
+
+			assert.Equal(t, "Material ya asociado al rubro", mapBody["message"])
+		})
 	})
 }
