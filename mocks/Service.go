c86ae4a06bd2ae8cafdea3aaf373fbd80345784a
Andres Court <andres@andrescourt.com>
2026-01-05 21:29:43 -0500

    test(budgets): update the database service mocks

--- mocks/Service.go
+++ mocks/Service.go
@@ -607,6 +607,63 @@ func (_c *Service_GetBudgetItemsByAccumulate_Call) RunAndReturn(run func(bool) (
 	return _c
 }
 
+// GetBudgets provides a mock function with no fields
+func (_m *Service) GetBudgets() ([]types.Budget, error) {
+	ret := _m.Called()
+
+	if len(ret) == 0 {
+		panic("no return value specified for GetBudgets")
+	}
+
+	var r0 []types.Budget
+	var r1 error
+	if rf, ok := ret.Get(0).(func() ([]types.Budget, error)); ok {
+		return rf()
+	}
+	if rf, ok := ret.Get(0).(func() []types.Budget); ok {
+		r0 = rf()
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).([]types.Budget)
+		}
+	}
+
+	if rf, ok := ret.Get(1).(func() error); ok {
+		r1 = rf()
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// Service_GetBudgets_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'GetBudgets'
+type Service_GetBudgets_Call struct {
+	*mock.Call
+}
+
+// GetBudgets is a helper method to define mock.On call
+func (_e *Service_Expecter) GetBudgets() *Service_GetBudgets_Call {
+	return &Service_GetBudgets_Call{Call: _e.mock.On("GetBudgets")}
+}
+
+func (_c *Service_GetBudgets_Call) Run(run func()) *Service_GetBudgets_Call {
+	_c.Call.Run(func(args mock.Arguments) {
+		run()
+	})
+	return _c
+}
+
+func (_c *Service_GetBudgets_Call) Return(_a0 []types.Budget, _a1 error) *Service_GetBudgets_Call {
+	_c.Call.Return(_a0, _a1)
+	return _c
+}
+
+func (_c *Service_GetBudgets_Call) RunAndReturn(run func() ([]types.Budget, error)) *Service_GetBudgets_Call {
+	_c.Call.Return(run)
+	return _c
+}
+
 // GetCategories provides a mock function with no fields
 func (_m *Service) GetCategories() ([]types.Category, error) {
 	ret := _m.Called()
