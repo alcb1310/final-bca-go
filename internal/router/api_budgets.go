41e01029108adfb2679663631a130929b32dd354
Andres Court <andres@andrescourt.com>
2026-01-06 21:16:44 -0500

    feat(budget): get all budgets

--- internal/router/api_budgets.go
+++ internal/router/api_budgets.go
@@ -1,7 +1,24 @@
 package router
 
-import "net/http"
+import (
+	"encoding/json"
+	"log/slog"
+	"net/http"
+
+	"github.com/alcb1310/final-bca-go/internal/types"
+)
 
 func (rf *Router) GetBudgets(w http.ResponseWriter, r *http.Request) {
+	var err error
+	budgets := []types.Budget{}
+
+	if budgets, err = rf.DB.GetBudgets(); err != nil {
+		slog.Error(err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		_ = json.NewEncoder(w).Encode(err)
+		return
+	}
+
 	w.WriteHeader(http.StatusNotImplemented)
+	_ = json.NewEncoder(w).Encode(budgets)
 }
