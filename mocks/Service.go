6ce2dc32fc676c36fb78021b99223600cadf256f
Andres Court <andres@andrescourt.com>
2026-01-13 18:49:48 -0500

    test(invoices): update the database service mocks

--- mocks/Service.go
+++ mocks/Service.go
@@ -925,6 +925,63 @@ func (_c *Service_GetHealth_Call) RunAndReturn(run func() bool) *Service_GetHeal
 	return _c
 }
 
+// GetInvoices provides a mock function with no fields
+func (_m *Service) GetInvoices() ([]types.InvoiceResponse, error) {
+	ret := _m.Called()
+
+	if len(ret) == 0 {
+		panic("no return value specified for GetInvoices")
+	}
+
+	var r0 []types.InvoiceResponse
+	var r1 error
+	if rf, ok := ret.Get(0).(func() ([]types.InvoiceResponse, error)); ok {
+		return rf()
+	}
+	if rf, ok := ret.Get(0).(func() []types.InvoiceResponse); ok {
+		r0 = rf()
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).([]types.InvoiceResponse)
+		}
+	}
+
+	if rf, ok := ret.Get(1).(func() error); ok {
+		r1 = rf()
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// Service_GetInvoices_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'GetInvoices'
+type Service_GetInvoices_Call struct {
+	*mock.Call
+}
+
+// GetInvoices is a helper method to define mock.On call
+func (_e *Service_Expecter) GetInvoices() *Service_GetInvoices_Call {
+	return &Service_GetInvoices_Call{Call: _e.mock.On("GetInvoices")}
+}
+
+func (_c *Service_GetInvoices_Call) Run(run func()) *Service_GetInvoices_Call {
+	_c.Call.Run(func(args mock.Arguments) {
+		run()
+	})
+	return _c
+}
+
+func (_c *Service_GetInvoices_Call) Return(_a0 []types.InvoiceResponse, _a1 error) *Service_GetInvoices_Call {
+	_c.Call.Return(_a0, _a1)
+	return _c
+}
+
+func (_c *Service_GetInvoices_Call) RunAndReturn(run func() ([]types.InvoiceResponse, error)) *Service_GetInvoices_Call {
+	_c.Call.Return(run)
+	return _c
+}
+
 // GetItem provides a mock function with given fields: id
 func (_m *Service) GetItem(id uuid.UUID) (types.Items, error) {
 	ret := _m.Called(id)
