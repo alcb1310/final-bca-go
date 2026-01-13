7b30654f3fccb09ae46dd7cd3513c603518162f7
Andres Court <andres@andrescourt.com>
2026-01-13 18:30:56 -0500

    feat(invoices): added the invoice response type

---
+++ internal/types/invoices.go
@@ -0,0 +1,17 @@
+package types
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type InvoiceResponse struct {
+	Id            uuid.UUID `json:"id"`
+	Supplier      Supplier  `json:"supplier"`
+	Project       Project   `json:"project"`
+	InvoiceNumber string    `json:"invoice_number"`
+	InvoiceDate   time.Time `json:"invoice_date"`
+	InvoiceTotal  float64   `json:"invoice_total"`
+	IsBalanced    bool      `json:"is_balanced"`
+}
