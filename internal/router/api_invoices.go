aee48aeb109271159a30eba8251508b55a25c2c7
Andres Court <andres@andrescourt.com>
2026-01-14 07:18:54 -0500

    feat(invoices): get all invoices

--- internal/router/api_invoices.go
+++ internal/router/api_invoices.go
@@ -1,7 +1,22 @@
 package router
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/alcb1310/final-bca-go/internal/types"
+)
 
 func (rf *Router) GetInvoices(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusNotImplemented)
+	invoices := []types.InvoiceResponse{}
+	var err error
+
+	if invoices, err = rf.DB.GetInvoices(); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		_ = json.NewEncoder(w).Encode(err)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	_ = json.NewEncoder(w).Encode(invoices)
 }
