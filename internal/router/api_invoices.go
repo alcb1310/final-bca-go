1f53e4c0d5379e3c174c1cf0838de8e0ddb13984
Andres Court <andres@andrescourt.com>
2026-01-15 17:51:56 -0500

    feat(invoices): read the user body

--- internal/router/api_invoices.go
+++ internal/router/api_invoices.go
@@ -22,5 +22,26 @@ func (rf *Router) GetInvoices(w http.ResponseWriter, r *http.Request) {
 }
 
 func (rf *Router) CreateInvoice(w http.ResponseWriter, r *http.Request) {
+	errorResponse := make(map[string]any)
+	p := make(map[string]any)
+	var err error
+
+	if r.Body == http.NoBody || r.Body == nil {
+		w.WriteHeader(http.StatusUnprocessableEntity)
+		errorResponse["message"] = "Falta el cuerpo de la solicitud"
+		_ = json.NewEncoder(w).Encode(errorResponse)
+		return
+	}
+
+	if err = json.NewDecoder(r.Body).Decode(&p); err != nil {
+		errorResponse["message"] = "Cuerpo de la solicitud no válido"
+	}
+
+	if len(errorResponse) > 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		_ = json.NewEncoder(w).Encode(errorResponse)
+		return
+	}
+
 	w.WriteHeader(http.StatusNotImplemented)
 }
