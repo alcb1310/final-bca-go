45e3315c710822767238d1eb1f111255880c0f24
Andres Court <andres@andrescourt.com>
2026-01-17 10:56:44 -0500

    test(invoices): add the suppliers seed data

---
+++ tests/scripts/seed_suppliers.sql
@@ -0,0 +1,17 @@
+INSERT INTO supplier (
+    id,
+    name,
+    supplier_id
+) VALUES (
+    '2da67854-8d6b-4787-a2ce-bde7e07eb1c3',
+    'Supplier Name 1',
+    'supplier-1'
+), (
+    '2da67854-8d6b-4787-a2ce-bde7e07eb1c4',
+    'Supplier Name 2',
+    'supplier-2'
+), (
+    '2da67854-8d6b-4787-a2ce-bde7e07eb1c5',
+    'Supplier Name 3',
+    'supplier-3'
+)
