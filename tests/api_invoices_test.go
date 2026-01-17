b82ce8f2a212d1a36b761ad9d7af972dc0c37cb3
Andres Court <andres@andrescourt.com>
2026-01-17 12:48:09 -0500

    test(invoices): should be able to create an invoice

--- tests/api_invoices_test.go
+++ tests/api_invoices_test.go
@@ -3,6 +3,7 @@ package tests
 import (
 	"context"
 	"encoding/json"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"path/filepath"
@@ -71,4 +72,59 @@ func TestApiInvoicesTest(t *testing.T) {
 		assert.Equal(t, 0, len(r))
 		assert.Equal(t, "[]", strings.TrimSpace(res.Body.String()))
 	})
+
+	t.Run("should be able to create an invoice", func(t *testing.T) {
+		form := map[string]any{
+			"project_id":     "1c6020db-39a0-451d-89ee-fdd20d519828",
+			"supplier_id":    "2da67854-8d6b-4787-a2ce-bde7e07eb1c4",
+			"invoice_number": "100-100-100",
+			"invoice_date":   "2022-01-01",
+		}
+
+		j, err := json.Marshal(form)
+		assert.NoError(t, err)
+
+		req, err := http.NewRequest("POST", testUrl, strings.NewReader(string(j)))
+		assert.NoError(t, err)
+		req.Header.Set("Content-Type", "application/json")
+		res := httptest.NewRecorder()
+		s.Router.ServeHTTP(res, req)
+
+		assert.Equal(t, http.StatusCreated, res.Code)
+
+		body, err := io.ReadAll(res.Body)
+		assert.NoError(t, err)
+		mapBody := make(map[string]any)
+		err = json.Unmarshal(body, &mapBody)
+		assert.NoError(t, err)
+
+		assert.Equal(t, "Factura creada", mapBody["message"])
+
+		req, err = http.NewRequest("GET", testUrl, nil)
+		assert.NoError(t, err)
+		req.Header.Set("Content-Type", "application/json")
+		res = httptest.NewRecorder()
+		s.Router.ServeHTTP(res, req)
+
+		var r []any
+		err = json.Unmarshal(res.Body.Bytes(), &r)
+		t.Log(r)
+		assert.NoError(t, err)
+		assert.Equal(t, 1, len(r))
+		for _, v := range r {
+			assert.Equal(t, "100-100-100", v.(map[string]any)["invoice_number"])
+			assert.Equal(t, "2022-01-01T00:00:00Z", v.(map[string]any)["invoice_date"])
+			assert.Equal(t, float64(0), v.(map[string]any)["invoice_total"])
+			assert.Equal(t, false, v.(map[string]any)["is_balanced"])
+
+			project := v.(map[string]any)["project"].(map[string]any)
+			assert.Equal(t, "Project 2", project["name"])
+			assert.Equal(t, "1c6020db-39a0-451d-89ee-fdd20d519828", project["id"])
+
+			supplier := v.(map[string]any)["supplier"].(map[string]any)
+			assert.Equal(t, "Supplier Name 2", supplier["name"])
+			assert.Equal(t, "2da67854-8d6b-4787-a2ce-bde7e07eb1c4", supplier["id"])
+		}
+	})
+
 }
