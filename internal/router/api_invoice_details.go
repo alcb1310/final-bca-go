4afa684a1ff4f08353025a1100eb647e77847cf1
Andres Court <andres@andrescourt.com>
2026-01-23 16:36:58 -0500

    feat(invoice details): read and validate the invoice id

--- internal/router/api_invoice_details.go
+++ internal/router/api_invoice_details.go
@@ -1,7 +1,37 @@
 package router
 
-import "net/http"
+import (
+	"database/sql"
+	"encoding/json"
+	"net/http"
+
+	"github.com/alcb1310/final-bca-go/internal/types"
+	"github.com/go-chi/chi/v5"
+	"github.com/google/uuid"
+)
 
 func (rf *Router) GetInvoiceDetails(w http.ResponseWriter, r *http.Request) {
+	pId := chi.URLParam(r, "projectId")
+	parsedId, err := uuid.Parse(pId)
+	inv := types.InvoiceResponse{}
+	if err != nil {
+		w.WriteHeader(http.StatusNotAcceptable)
+		_ = json.NewEncoder(w).Encode(map[string]any{"message": "Id inválido"})
+		return
+	}
+
+	if inv, err = rf.DB.GetInvoice(parsedId); err != nil {
+		if err == sql.ErrNoRows {
+			w.WriteHeader(http.StatusNotFound)
+			_ = json.NewEncoder(w).Encode(map[string]any{"message": "Factura no encontrada"})
+			return
+		}
+
+		w.WriteHeader(http.StatusInternalServerError)
+		_ = json.NewEncoder(w).Encode(err)
+		return
+	}
+
 	w.WriteHeader(http.StatusNotImplemented)
+	_ = json.NewEncoder(w).Encode(inv)
 }
