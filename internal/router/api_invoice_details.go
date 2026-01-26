48be4b7c34cb5dbedf8f48c9eff2e74f91fdebf1
Andres Court <andres@andrescourt.com>
2026-01-25 20:08:22 -0500

    feat(invoice-details): call the get invoice details function

--- internal/router/api_invoice_details.go
+++ internal/router/api_invoice_details.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"net/http"
 
-	"github.com/alcb1310/final-bca-go/internal/types"
 	"github.com/go-chi/chi/v5"
 	"github.com/google/uuid"
 )
@@ -13,14 +12,13 @@ import (
 func (rf *Router) GetInvoiceDetails(w http.ResponseWriter, r *http.Request) {
 	pId := chi.URLParam(r, "projectId")
 	parsedId, err := uuid.Parse(pId)
-	inv := types.InvoiceResponse{}
 	if err != nil {
 		w.WriteHeader(http.StatusNotAcceptable)
 		_ = json.NewEncoder(w).Encode(map[string]any{"message": "Id inválido"})
 		return
 	}
 
-	if inv, err = rf.DB.GetInvoice(parsedId); err != nil {
+	if _, err = rf.DB.GetInvoice(parsedId); err != nil {
 		if err == sql.ErrNoRows {
 			w.WriteHeader(http.StatusNotFound)
 			_ = json.NewEncoder(w).Encode(map[string]any{"message": "Factura no encontrada"})
@@ -32,6 +30,13 @@ func (rf *Router) GetInvoiceDetails(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.WriteHeader(http.StatusNotImplemented)
-	_ = json.NewEncoder(w).Encode(inv)
+	invoiceDetails, err := rf.DB.GetInvoiceDetails(parsedId)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		_ = json.NewEncoder(w).Encode(err)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	_ = json.NewEncoder(w).Encode(invoiceDetails)
 }
