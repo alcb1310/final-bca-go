2d5a04d399a44a8e1992f57c70d6ee50663120db
Andres Court <andres@andrescourt.com>
2026-02-23 18:44:27 -0500

    feat(invoice-details): added the create struct

--- internal/types/invoice-details.go
+++ internal/types/invoice-details.go
@@ -1,5 +1,7 @@
 package types
 
+import "github.com/google/uuid"
+
 type InvoiceDetailsResponse struct {
 	Invoice    InvoiceResponse `json:"invoice"`
 	Project    Project         `json:"project"`
@@ -8,4 +10,11 @@ type InvoiceDetailsResponse struct {
 	Quantity   float64         `json:"quantity"`
 	Cost       float64         `json:"cost"`
 	Total      float64         `json:"total"`
+}
+
+type InvoiceDetailsCreate struct {
+	InvoiceId    uuid.UUID `json:"invoice_id"`
+	BudgetItemId uuid.UUID `json:"budget_item_id"`
+	Quantity     float64   `json:"quantity"`
+	Cost         float64   `json:"cost"`
 }
