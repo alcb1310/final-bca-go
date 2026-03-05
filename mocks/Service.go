507a61477ed1ed8071309a78750f985fef573203
Andres Court <andres@andrescourt.com>
2026-03-04 20:23:37 -0500

    test(invoice-details): update the database service mocks

--- mocks/Service.go
+++ mocks/Service.go
@@ -205,6 +205,52 @@ func (_c *Service_CreateInvoice_Call) RunAndReturn(run func(*types.InvoiceCreate
 	return _c
 }
 
+// CreateInvoiceDetail provides a mock function with given fields: detail
+func (_m *Service) CreateInvoiceDetail(detail types.InvoiceDetailsCreate) error {
+	ret := _m.Called(detail)
+
+	if len(ret) == 0 {
+		panic("no return value specified for CreateInvoiceDetail")
+	}
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(types.InvoiceDetailsCreate) error); ok {
+		r0 = rf(detail)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
+}
+
+// Service_CreateInvoiceDetail_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'CreateInvoiceDetail'
+type Service_CreateInvoiceDetail_Call struct {
+	*mock.Call
+}
+
+// CreateInvoiceDetail is a helper method to define mock.On call
+//   - detail types.InvoiceDetailsCreate
+func (_e *Service_Expecter) CreateInvoiceDetail(detail interface{}) *Service_CreateInvoiceDetail_Call {
+	return &Service_CreateInvoiceDetail_Call{Call: _e.mock.On("CreateInvoiceDetail", detail)}
+}
+
+func (_c *Service_CreateInvoiceDetail_Call) Run(run func(detail types.InvoiceDetailsCreate)) *Service_CreateInvoiceDetail_Call {
+	_c.Call.Run(func(args mock.Arguments) {
+		run(args[0].(types.InvoiceDetailsCreate))
+	})
+	return _c
+}
+
+func (_c *Service_CreateInvoiceDetail_Call) Return(_a0 error) *Service_CreateInvoiceDetail_Call {
+	_c.Call.Return(_a0)
+	return _c
+}
+
+func (_c *Service_CreateInvoiceDetail_Call) RunAndReturn(run func(types.InvoiceDetailsCreate) error) *Service_CreateInvoiceDetail_Call {
+	_c.Call.Return(run)
+	return _c
+}
+
 // CreateItem provides a mock function with given fields: item
 func (_m *Service) CreateItem(item *types.Items) error {
 	ret := _m.Called(item)
