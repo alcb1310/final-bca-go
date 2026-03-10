f3dafc14715b079755bb4746b6460692e037a095
Andres Court <andres@andrescourt.com>
2026-03-10 09:25:42 -0500

    test(invoice-details): should delete an invoice detail

--- tests/api_invoice_details_test.go
+++ tests/api_invoice_details_test.go
@@ -230,4 +230,121 @@ func TestApiInvoiceDetails(t *testing.T) {
 		assert.Equal(t, http.StatusConflict, resp.Code)
 		assert.Equal(t, "{\"message\":\"El detalle de la factura ya existe\"}", strings.TrimSpace(resp.Body.String()))
 	})
+
+	t.Run("should delete an invoice detail", func(t *testing.T) {
+		budgetResponse := []types.Budget{
+			{
+				Project:           types.Project{Id: uuid.MustParse("2118e27d-1ae5-4554-b0ba-2503917a31aa")},
+				BudgetItem:        types.BudgetItem{Id: uuid.MustParse("420f8bb3-bc8e-4564-be99-75cd7c1a6ff8")},
+				InitialQuantity:   sql.NullFloat64{Float64: 0, Valid: false},
+				InitialCost:       sql.NullFloat64{Float64: 0, Valid: false},
+				InitialTotal:      4567.5,
+				SpentQuantity:     sql.NullFloat64{Float64: 0, Valid: false},
+				SpentTotal:        0,
+				RemainingQuantity: sql.NullFloat64{Float64: 0, Valid: false},
+				RemainingCost:     sql.NullFloat64{Float64: 0, Valid: false},
+				RemainingTotal:    4567.5,
+				UpdatedBudget:     4567.5,
+			},
+			{
+				Project:           types.Project{Id: uuid.MustParse("2118e27d-1ae5-4554-b0ba-2503917a31aa")},
+				BudgetItem:        types.BudgetItem{Id: uuid.MustParse("9abc2426-a92b-46ef-b074-ddbc8ee2df1a")},
+				InitialQuantity:   sql.NullFloat64{Float64: 2537.5, Valid: true},
+				InitialCost:       sql.NullFloat64{Float64: 1.8, Valid: true},
+				InitialTotal:      4567.5,
+				SpentQuantity:     sql.NullFloat64{Float64: 0, Valid: true},
+				SpentTotal:        0,
+				RemainingQuantity: sql.NullFloat64{Float64: 2537.5, Valid: true},
+				RemainingCost:     sql.NullFloat64{Float64: 1.8, Valid: true},
+				RemainingTotal:    4567.5,
+				UpdatedBudget:     4567.5,
+			},
+			{
+				Project:           types.Project{Id: uuid.MustParse("2118e27d-1ae5-4554-b0ba-2503917a31aa")},
+				BudgetItem:        types.BudgetItem{Id: uuid.MustParse("439082ad-f1bd-4228-91f2-8e744894ffdc")},
+				InitialQuantity:   sql.NullFloat64{Float64: 0, Valid: false},
+				InitialCost:       sql.NullFloat64{Float64: 0, Valid: false},
+				InitialTotal:      100.0,
+				SpentQuantity:     sql.NullFloat64{Float64: 0, Valid: false},
+				SpentTotal:        0.0,
+				RemainingQuantity: sql.NullFloat64{Float64: 0, Valid: false},
+				RemainingCost:     sql.NullFloat64{Float64: 0, Valid: false},
+				RemainingTotal:    100.0,
+				UpdatedBudget:     100.0,
+			},
+			{
+				Project:           types.Project{Id: uuid.MustParse("2118e27d-1ae5-4554-b0ba-2503917a31aa")},
+				BudgetItem:        types.BudgetItem{Id: uuid.MustParse("b4b2e4e4-f22d-402e-9ab5-1d59347cbfcb")},
+				InitialQuantity:   sql.NullFloat64{Float64: 4, Valid: true},
+				InitialCost:       sql.NullFloat64{Float64: 25, Valid: true},
+				InitialTotal:      100.0,
+				SpentQuantity:     sql.NullFloat64{Float64: 0, Valid: true},
+				SpentTotal:        0.0,
+				RemainingQuantity: sql.NullFloat64{Float64: 4, Valid: true},
+				RemainingCost:     sql.NullFloat64{Float64: 25, Valid: true},
+				RemainingTotal:    100.0,
+				UpdatedBudget:     100.0,
+			},
+			{
+				Project:           types.Project{Id: uuid.MustParse("1c6020db-39a0-451d-89ee-fdd20d519828")},
+				BudgetItem:        types.BudgetItem{Id: uuid.MustParse("439082ad-f1bd-4228-91f2-8e744894ffdc")},
+				InitialQuantity:   sql.NullFloat64{Float64: 0, Valid: false},
+				InitialCost:       sql.NullFloat64{Float64: 0, Valid: false},
+				InitialTotal:      100,
+				SpentQuantity:     sql.NullFloat64{Float64: 0, Valid: false},
+				SpentTotal:        0,
+				RemainingQuantity: sql.NullFloat64{Float64: 0, Valid: false},
+				RemainingCost:     sql.NullFloat64{Float64: 0, Valid: false},
+				RemainingTotal:    100,
+				UpdatedBudget:     100,
+			},
+			{
+				Project:           types.Project{Id: uuid.MustParse("1c6020db-39a0-451d-89ee-fdd20d519828")},
+				BudgetItem:        types.BudgetItem{Id: uuid.MustParse("b4b2e4e4-f22d-402e-9ab5-1d59347cbfcb")},
+				InitialQuantity:   sql.NullFloat64{Float64: 4, Valid: true},
+				InitialCost:       sql.NullFloat64{Float64: 25, Valid: true},
+				InitialTotal:      100,
+				SpentQuantity:     sql.NullFloat64{Float64: 0, Valid: true},
+				SpentTotal:        0,
+				RemainingQuantity: sql.NullFloat64{Float64: 4, Valid: true},
+				RemainingCost:     sql.NullFloat64{Float64: 25, Valid: true},
+				RemainingTotal:    100,
+				UpdatedBudget:     100,
+			},
+		}
+
+		form := map[string]any{}
+		j, err := json.Marshal(form)
+		assert.NoError(t, err)
+
+		req, err := http.NewRequest(
+			http.MethodDelete,
+			fmt.Sprintf("%s/%s", testURL, "b4b2e4e4-f22d-402e-9ab5-1d59347cbfcb"),
+			strings.NewReader(string(j)),
+		)
+		assert.NoError(t, err)
+		req.Header.Add("Content-Type", "application/json")
+		resp := httptest.NewRecorder()
+		s.Router.ServeHTTP(resp, req)
+		assert.NoError(t, err)
+		assert.Equal(t, http.StatusNoContent, resp.Code)
+
+		budgets, err := s.DB.GetBudgets()
+		assert.NoError(t, err)
+		assert.Equal(t, 6, len(budgets))
+
+		for i, b := range budgets {
+			assert.Equal(t, b.Project.Id, budgetResponse[i].Project.Id)
+			assert.Equal(t, b.BudgetItem.Id, budgetResponse[i].BudgetItem.Id)
+			assert.Equal(t, b.InitialQuantity, budgetResponse[i].InitialQuantity)
+			assert.Equal(t, b.InitialCost, budgetResponse[i].InitialCost)
+			assert.Equal(t, b.InitialTotal, budgetResponse[i].InitialTotal)
+			assert.Equal(t, b.SpentQuantity, budgetResponse[i].SpentQuantity)
+			assert.Equal(t, b.SpentTotal, budgetResponse[i].SpentTotal)
+			assert.Equal(t, b.RemainingQuantity, budgetResponse[i].RemainingQuantity)
+			assert.Equal(t, b.RemainingCost, budgetResponse[i].RemainingCost)
+			assert.Equal(t, b.RemainingTotal, budgetResponse[i].RemainingTotal)
+			assert.Equal(t, b.UpdatedBudget, budgetResponse[i].UpdatedBudget)
+		}
+	})
 }
