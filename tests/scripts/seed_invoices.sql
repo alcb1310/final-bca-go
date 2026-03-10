3ee410b0b32cb355bfdbec8b82dd15e043646fe7
Andres Court <andres@andrescourt.com>
2026-03-09 21:25:33 -0500

    test(seed): invoice information seed data

---
+++ tests/scripts/seed_invoices.sql
@@ -0,0 +1,8 @@
+insert into invoice(id, supplier_id, project_id, invoice_number, invoice_date) values
+(
+    'c3be2956-1c3c-46f7-af14-d28420116f14',
+    '2da67854-8d6b-4787-a2ce-bde7e07eb1c4',
+    '2118e27d-1ae5-4554-b0ba-2503917a31aa',
+    '001-001-11',
+    '2024-08-24'
+);
