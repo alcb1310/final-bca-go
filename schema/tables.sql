e155817a06324ebd82ae745bf457439e6c003d7b
Andres Court <andres@andrescourt.com>
2026-03-23 16:11:47 -0500

    feat(closure): created the historic table schema
    Also added the SQL LSP and had some formating changes

--- schema/tables.sql
+++ schema/tables.sql
@@ -1,185 +1,191 @@
-create table if not exists project (
-    id uuid primary key default gen_random_uuid(),
-    name text not null,
-    is_active boolean not null,
-    gross_area numeric not null default 0,
-    net_area numeric not null default 0,
-    last_closure date default null,
-    created_at timestamp default now(),
-
-    unique (name)
+CREATE TABLE if NOT EXISTS project(
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name TEXT NOT NULL,
+    is_active BOOLEAN NOT NULL,
+    gross_area NUMERIC NOT NULL DEFAULT 0,
+    net_area NUMERIC NOT NULL DEFAULT 0,
+    last_closure DATE DEFAULT NULL,
+    created_at TIMESTAMP DEFAULT now() UNIQUE(name)
 );
-
-create table if not exists supplier (
-    id uuid primary key default gen_random_uuid(),
-    name text not null,
-    supplier_id text not null,
-    contact_name text,
-    contact_email text,
-    contact_phone text,
-    created_at timestamp default now(),
-
-    unique (supplier_id),
-    unique (name)
+CREATE TABLE if NOT EXISTS supplier(
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name TEXT NOT NULL,
+    supplier_id TEXT NOT NULL,
+    contact_name TEXT,
+    contact_email TEXT,
+    contact_phone TEXT,
+    created_at TIMESTAMP DEFAULT now() UNIQUE(supplier_id),
+    UNIQUE(name)
 );
-
-create table if not exists budget_item(
-    id uuid primary key default gen_random_uuid(),
-    code text not null,
-    name text not null,
-    level smallint not null default 1,
-    accumulate boolean not null default true,
-    parent_id uuid references budget_item(id) on delete restrict,
-    created_at timestamp default now(),
-
-    unique (code),
-    unique (name)
+CREATE TABLE if NOT EXISTS budget_item(
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    code TEXT NOT NULL,
+    name TEXT NOT NULL,
+    level SMALLINT NOT NULL DEFAULT 1,
+    accumulate BOOLEAN NOT NULL DEFAULT TRUE,
+    parent_id UUID REFERENCES budget_item(id)
+ON DELETE restrict,
+    created_at TIMESTAMP DEFAULT now(),
+    UNIQUE(code),
+    UNIQUE(name)
 );
-
-create table if not exists category(
-    id uuid primary key default gen_random_uuid(),
-    name text not null,
-    created_at timestamp default now(),
-
-    unique (name)
+CREATE TABLE if NOT EXISTS category(
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT now(),
+    UNIQUE(name)
 );
-
-create table if not exists material(
-    id uuid primary key default gen_random_uuid(),
-    code text not null,
-    name text not null,
-    unit text not null,
-
-    category_id uuid references category(id) on delete restrict,
-    created_at timestamp default now(),
-
-    unique (name),
-    unique (code)
+CREATE TABLE if NOT EXISTS material(
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    code TEXT NOT NULL,
+    name TEXT NOT NULL,
+    unit TEXT NOT NULL,
+    category_id UUID REFERENCES category(id)
+ON DELETE restrict,
+    created_at TIMESTAMP DEFAULT now(),
+    UNIQUE(name),
+    UNIQUE(code)
 );
-
-create table if not exists item(
-    id uuid primary key default gen_random_uuid(),
-    code text not null,
-    name text not null,
-    unit text not null,
-
-    created_at timestamp default now(),
-
-    unique (name),
-    unique (code)
+CREATE TABLE if NOT EXISTS item(
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    code TEXT NOT NULL,
+    name TEXT NOT NULL,
+    unit TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT now(),
+    UNIQUE(name),
+    UNIQUE(code)
 );
-
-create table if not exists item_material(
-    item_id uuid references item(id) on delete restrict,
-    material_id uuid references material(id) on delete restrict,
-    quantity numeric not null default 0,
-    created_at timestamp default now(),
-
-    primary key (item_id, material_id)
+CREATE TABLE if NOT EXISTS item_material(
+    item_id UUID REFERENCES item(id)
+ON DELETE restrict,
+    material_id UUID REFERENCES material(id)
+ON DELETE restrict,
+    quantity NUMERIC NOT NULL DEFAULT 0,
+    created_at TIMESTAMP DEFAULT now(),
+    PRIMARY KEY(item_id,
+    material_id)
 );
-
-create table if not exists budget(
-    project_id uuid references project(id) on delete restrict,
-    budget_item_id uuid references budget_item(id) on delete restrict,
-
-    initial_quantity numeric,
-    initial_cost numeric,
-    initial_total numeric not null,
-
-    spent_quantity numeric,
-    spent_total numeric not null,
-
-    remaining_quantity numeric,
-    remaining_cost numeric,
-    remaining_total numeric not null,
-
-    updated_budget numeric not null,
-
-    created_at timestamp default now(),
-
-    primary key (project_id, budget_item_id)
+CREATE TABLE if NOT EXISTS budget(
+    project_id UUID REFERENCES project(id)
+ON DELETE restrict,
+    budget_item_id UUID REFERENCES budget_item(id)
+ON DELETE restrict,
+    initial_quantity NUMERIC,
+    initial_cost NUMERIC,
+    initial_total NUMERIC NOT NULL,
+    spent_quantity NUMERIC,
+    spent_total NUMERIC NOT NULL,
+    remaining_quantity NUMERIC,
+    remaining_cost NUMERIC,
+    remaining_total NUMERIC NOT NULL,
+    updated_budget NUMERIC NOT NULL,
+    created_at TIMESTAMP DEFAULT now(),
+    PRIMARY KEY(project_id,
+    budget_item_id)
 );
-
-create table if not exists invoice(
-    id uuid primary key default gen_random_uuid(),
-    project_id uuid references project(id) on delete restrict,
-    supplier_id uuid references supplier(id) on delete restrict,
-    invoice_number text not null,
-    invoice_date date not null,
-    invoice_total numeric not null,
-    is_balanced boolean not null default false,
-    created_at timestamp default now(),
-
-    unique (supplier_id, project_id, invoice_number)
+CREATE TABLE if NOT EXISTS invoice(
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    project_id UUID REFERENCES project(id)
+ON DELETE restrict,
+    supplier_id UUID REFERENCES supplier(id)
+ON DELETE restrict,
+    invoice_number TEXT NOT NULL,
+    invoice_date DATE NOT NULL,
+    invoice_total NUMERIC NOT NULL,
+    is_balanced BOOLEAN NOT NULL DEFAULT FALSE,
+    created_at TIMESTAMP DEFAULT now(),
+    UNIQUE(supplier_id,
+    project_id,
+    invoice_number)
 );
-
-alter table invoice alter column invoice_total set default 0;
-
-create table if not exists invoice_details (
-     invoice_id uuid not null references invoice (id) on delete restrict,
-     budget_item_id uuid not null references budget_item (id) on delete restrict,
-     quantity numeric not null,
-     cost numeric not null,
-     total numeric not null,
-     created_at timestamp with time zone default now(),
-
-     unique (invoice_id, budget_item_id),
-     primary key (invoice_id, budget_item_id)
+ALTER TABLE invoice ALTER COLUMN invoice_total
+SET DEFAULT 0;
+CREATE TABLE if NOT EXISTS invoice_details(
+    invoice_id UUID NOT NULL REFERENCES invoice(id)
+ON DELETE restrict,
+    budget_item_id UUID NOT NULL REFERENCES budget_item(id)
+ON DELETE restrict,
+    quantity NUMERIC NOT NULL,
+    cost NUMERIC NOT NULL,
+    total NUMERIC NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT now() UNIQUE(invoice_id,
+    budget_item_id),
+    PRIMARY KEY(invoice_id,
+    budget_item_id)
 );
-
-
+CREATE TABLE if NOT EXISTS historic(
+    project_id UUID NOT NULL REFERENCES project(id)
+ON DELETE restrict,
+    budget_item_id UUID NOT NULL REFERENCES budget_item(id)
+ON DELETE restrict DATE DATE NOT NULL,
+    initial_quantity NUMERIC,
+    initial_cost NUMERIC,
+    initial_total NUMERIC NOT NULL,
+    spent_quantity NUMERIC,
+    spent_total NUMERIC NOT NULL,
+    remaining_quantity NUMERIC,
+    remaining_cost NUMERIC,
+    remaining_total NUMERIC NOT NULL,
+    updated_budget NUMERIC NOT NULL,
+    created_at TIMESTAMP DEFAULT now(),
+    PRIMARY KEY(project_id,
+    budget_item_id DATE)
+);
 ----------------------------------------
 --                VIEWS               --
 ----------------------------------------
-
-create or replace view vw_budget_item as
-select
+CREATE
+OR REPLACE VIEW vw_budget_item AS SELECT
     b.id,
     b.code,
     b.name,
     b.level,
     b.accumulate,
-    p.id as parent_id,
-    p.code as parent_code,
-    p.name as parent_name
-from budget_item b
-left join budget_item p on b.parent_id = p.id;
-
-create or replace view vw_materials as
-select
+    p.id AS parent_id,
+    p.code AS parent_code,
+    p.name AS parent_name
+FROM
+    budget_item b
+LEFT JOIN budget_item p
+    ON b.parent_id = p.id;
+CREATE
+OR REPLACE VIEW vw_materials AS SELECT
     m.id,
     m.code,
     m.name,
     m.unit,
-    c.id as category_id,
-    c.name as category_name
-from material m
-left join category c on m.category_id = c.id;
-
-create or replace view vw_item_materials as
-select
-    m.id as material_id,
-    m.code as material_code,
-    m.name as material_name,
-    m.unit as material_unit,
-    i.id as item_id,
-    i.code as item_code,
-    i.name as item_name,
-    i.unit as item_unit,
+    c.id AS category_id,
+    c.name AS category_name
+FROM
+    material m
+LEFT JOIN category c
+    ON m.category_id = c.id;
+CREATE
+OR REPLACE VIEW vw_item_materials AS SELECT
+    m.id AS material_id,
+    m.code AS material_code,
+    m.name AS material_name,
+    m.unit AS material_unit,
+    i.id AS item_id,
+    i.code AS item_code,
+    i.name AS item_name,
+    i.unit AS item_unit,
     im.quantity
-from item_material im
-left join material m on im.material_id = m.id
-left join item i on im.item_id = i.id;
-
-create or replace view vw_budget as
-select
-    bi.id as budget_item_id,
-    bi.code as budget_item_code,
-    bi.name as budget_item_name,
-    bi.level as budget_item_level,
-    bi.accumulate as budget_item_accumulate,
-    p.id as project_id,
-    p.name as project_name,
+FROM
+    item_material im
+LEFT JOIN material m
+    ON im.material_id = m.id
+LEFT JOIN item i
+    ON im.item_id = i.id;
+CREATE
+OR REPLACE VIEW vw_budget AS SELECT
+    bi.id AS budget_item_id,
+    bi.code AS budget_item_code,
+    bi.name AS budget_item_name,
+    bi.level AS budget_item_level,
+    bi.accumulate AS budget_item_accumulate,
+    p.id AS project_id,
+    p.name AS project_name,
     b.initial_quantity,
     b.initial_cost,
     b.initial_total,
@@ -189,51 +195,59 @@ select
     b.remaining_cost,
     b.remaining_total,
     b.updated_budget
-from budget b
-left join project p on b.project_id = p.id
-left join budget_item bi on b.budget_item_id = bi.id;
-
-create or replace view vw_invoice as
-select
+FROM
+    budget b
+LEFT JOIN project p
+    ON b.project_id = p.id
+LEFT JOIN budget_item bi
+    ON b.budget_item_id = bi.id;
+CREATE
+OR REPLACE VIEW vw_invoice AS SELECT
     i.id,
-    s.id as supplier_id,
-    s.supplier_id as supplier_number,
-    s.name as supplier_name,
+    s.id AS supplier_id,
+    s.supplier_id AS supplier_number,
+    s.name AS supplier_name,
     s.contact_name,
     s.contact_email,
     s.contact_phone,
-    p.id as project_id,
-    p.name as project_name,
+    p.id AS project_id,
+    p.name AS project_name,
     p.is_active,
     i.invoice_number,
     i.invoice_date,
     i.invoice_total,
     i.is_balanced
-from invoice i
-join supplier s on i.supplier_id = s.id
-join project p on i.project_id = p.id;
-
-create or replace view vw_invoice_details as 
-select
+FROM
+    invoice i
+JOIN supplier s
+    ON i.supplier_id = s.id
+JOIN project p
+    ON i.project_id = p.id;
+CREATE
+OR REPLACE VIEW vw_invoice_details AS SELECT
     id.invoice_id,
     i.invoice_number,
-    i.invoice_total as invoice_total,
-    i.invoice_date as invoice_date,
-    p.id as project_id,
-    p.name as project_name,
-    s.id as supplier_id,
-    s.supplier_id as supplier_number,
-    s.name as supplier_name,
+    i.invoice_total AS invoice_total,
+    i.invoice_date AS invoice_date,
+    p.id AS project_id,
+    p.name AS project_name,
+    s.id AS supplier_id,
+    s.supplier_id AS supplier_number,
+    s.name AS supplier_name,
     id.budget_item_id,
-    b.code as budget_item_code,
-    b.name as budget_item_name,
-    b.level as budget_item_level,
+    b.code AS budget_item_code,
+    b.name AS budget_item_name,
+    b.level AS budget_item_level,
     id.quantity,
     id.cost,
     id.total
-from invoice_details id
-join budget_item b on id.budget_item_id = b.id
-join invoice i on id.invoice_id = i.id
-join supplier s on i.supplier_id = s.id
-join project p on i.project_id = p.id;
-
+FROM
+    invoice_details id
+JOIN budget_item b
+    ON id.budget_item_id = b.id
+JOIN invoice i
+    ON id.invoice_id = i.id
+JOIN supplier s
+    ON i.supplier_id = s.id
+JOIN project p
+    ON i.project_id = p.id;
