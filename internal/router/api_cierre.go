765ea604cf366ce29f15e7e098d37a423acdbf3a
Andres Court <andres@andrescourt.com>
2026-03-24 23:36:11 -0500

    feat(closure): read the required data from the request

--- internal/router/api_cierre.go
+++ internal/router/api_cierre.go
@@ -3,9 +3,63 @@ package router
 import (
 	"encoding/json"
 	"net/http"
+	"time"
+
+	"github.com/google/uuid"
 )
 
 func (rf *Router) Closure(w http.ResponseWriter, r *http.Request) {
+	if r.Body == http.NoBody || r.Body == nil {
+		w.WriteHeader(http.StatusUnprocessableEntity)
+		_ = json.NewEncoder(w).Encode(map[string]any{"message": "Falta el cuerpo de la solicitud"})
+		return
+	}
+
+	var b map[string]any
+	if err := json.NewDecoder(r.Body).Decode(&b); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		_ = json.NewEncoder(w).Encode(map[string]any{"message": "Cuerpo de la solicitud no válido"})
+		return
+	}
+
+	var projectId uuid.UUID
+	var err error
+	var errorResponse = make(map[string]any)
+	var closureDate time.Time
+	var ok bool
+	var val string
+
+	if val, ok = b["project_id"].(string); !ok {
+		errorResponse["project_id"] = "El proyecto es obligatorio"
+	} else {
+		projectId, err = uuid.Parse(val)
+		if err != nil {
+			errorResponse["project_id"] = "El código del proyecto es inválido"
+		} else {
+			if _, err = rf.DB.GetProject(projectId); err != nil {
+				w.WriteHeader(http.StatusNotFound)
+				errorResponse["message"] = "El proyecto no existe"
+				_ = json.NewEncoder(w).Encode(errorResponse)
+				return
+			}
+		}
+	}
+
+	if val, ok = b["date"].(string); !ok {
+		errorResponse["date"] = "La fecha es obligatoria"
+	} else {
+		closureDate, err = time.ParseInLocation("2006-01-02", val, time.Local)
+		if err != nil {
+			errorResponse["date"] = "La fecha es inválida"
+		}
+	}
+
+	if len(errorResponse) > 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		_ = json.NewEncoder(w).Encode(errorResponse)
+		return
+	}
+
 	w.WriteHeader(http.StatusNotImplemented)
-	_ = json.NewEncoder(w).Encode(map[string]any{"message": "Not implemented"})
+	_ = json.NewEncoder(w).Encode(map[string]any{"message": "Not implemented", "project_id": projectId, "date": closureDate})
 }
